bdbdf959c3e76ea6b832866a768ddaeb9e68bfa1
pankajsahh <pankajsahu19056@gmail.com>
2022-06-27 19:47:59 +0530

    challenge done

--- src/component/Pages/General/General.jsx
+++ src/component/Pages/General/General.jsx
@@ -4,22 +4,16 @@ import Context from "../../context/Context";
 import "./General.css";
 
 const General = () => {
+  const { data, setdata } = useContext(Context);
 
-  const {data,setdata} = useContext(Context);
-
-
-
-  const handleChange = (e)=>{
+  const handleChange = (e) => {
     const Name = e.target.name;
     const Value = e.target.value;
     setdata({
       ...data,
-      [Name]:Value
-    })
-  }
-  
-
-
+      [Name]: Value,
+    });
+  };
 
   return (
     <div className="General">
@@ -28,34 +22,60 @@ const General = () => {
       </div>
       <div className="Generalbody">
         <div className="dateLocationName">
-          <input name="startDate" value={data.startDate} onChange={handleChange} type="date" placeholder="start date & time" />
-          <input name="location" value={data.location} onChange={handleChange} type="text" placeholder="match location " />
-         {data.Tournament_type==="Friendly"?
-        <div></div>
-      : 
-      <input
-      name="Tournament_Name"
+          <label htmlFor="name">Start Date</label>
+          <input
+            name="startDate"
+            value={data.startDate}
+            onChange={handleChange}
+            type="date"
+            placeholder="start date & time"
+          />
 
-      type="text"
-      value={data.Tournament_Name} onChange={handleChange}
-      placeholder="Tournament name"
-    />
-        } 
+          <input
+            name="location"
+            value={data.location}
+            onChange={handleChange}
+            type="text"
+            placeholder="match location "
+          />
+          {data.Tournament_type === "Friendly" ? (
+            <div></div>
+          ) : (
+            <input
+              name="Tournament_Name"
+              type="text"
+              value={data.Tournament_Name}
+              onChange={handleChange}
+              placeholder="Tournament name"
+            />
+          )}
         </div>
         <div className="endComment">
-          <input name="endDate" type="date"  value={data.endDate} onChange={handleChange} placeholder="end date & time" />
+          <label htmlFor="endDate">End Date</label>
+          <input
+            name="endDate"
+            type="date"
+            value={data.endDate}
+            onChange={handleChange}
+            placeholder="end date & time"
+          />
           <textarea
             name="comment"
             rows="10"
-            cols="50"
-            value={data.comment} onChange={handleChange}
+            cols="20"
+            value={data.comment}
+            onChange={handleChange}
             placeholder="comments"
           ></textarea>
         </div>
       </div>
       <div className="next">
-       <Link to="/team"><button>Next</button></Link> 
-       <Link to="/"><button>Back</button></Link> 
+        <Link to="/team">
+          <button>Next</button>
+        </Link>
+        <Link to="/">
+          <button>Back</button>
+        </Link>
       </div>
     </div>
   );
